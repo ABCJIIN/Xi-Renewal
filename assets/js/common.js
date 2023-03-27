484316089ff3a376981b560bda04e73ca0e21fe6
ABCJIIN <97517088+ABCJIIN@users.noreply.github.com>
2023-03-27 17:59:01 +0900

    0327/17:58/jinny

--- assets/js/common.js
+++ assets/js/common.js
@@ -2,4 +2,5 @@
 $(document).ready(function() {
     $('#header').load('/html/include/header.html');
     $('#footer').load('/html/include/footer.html');
+    $('#mainHeader').load('/html/include/main_header.html');
 })
